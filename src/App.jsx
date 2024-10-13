49c6e0309be38602b2aed5c052a3189860f871d3
VictorWFS <victorwfs900@gmail.com>
2024-10-13 15:57:03 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar"
+import NewsBoard from "./Components/NewsBoard"
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <NewsBoard/>
+    </div>
+  )
+}
+
+export default App
